ddac0d7cfd141dc701c5c46eadfbcfb133a05728
pangchao <bjpangchao@corp.netease.com>
2016-11-04 15:52:00 +0800

    添加最大子数组的和

--- src/main/java/cn/edu/bjtu/book/SubArray.java
+++ src/main/java/cn/edu/bjtu/book/SubArray.java
@@ -4,4 +4,27 @@ package cn.edu.bjtu.book;
  * Created by pangchao on 2016/11/4.
  */
 public class SubArray {
+    public static void main(String[] args) {
+        int[] array = {1, -2, 3, 10, -4, 7, 2, -5};
+        System.out.println(findMaxSubArraySum(array));
+    }
+
+    public static int findMaxSubArraySum(int[] array) {
+        if (array == null || array.length == 0)
+            return 0;
+        int curSum = 0;
+        int greatestSum = 0;
+        int len = array.length;
+        for (int i = 0; i < len; i++) {
+            if (curSum < 0)
+                curSum = array[i];
+            else
+                curSum += array[i];
+
+            if (curSum > greatestSum)
+                greatestSum = curSum;
+        }
+
+        return greatestSum;
+    }
 }
